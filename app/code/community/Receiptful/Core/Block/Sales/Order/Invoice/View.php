6cc44854162b9e0cb0f7572d1e736dedd7d1502b
Stefano Sala <stefano.sala@spa.it>
2015-01-14 16:20:59 +0100

    Created custom button

--- app/code/community/Receiptful/Core/Block/Sales/Order/Invoice/View.php
+++ app/code/community/Receiptful/Core/Block/Sales/Order/Invoice/View.php
@@ -5,6 +5,18 @@ class Receiptful_Core_Block_Sales_Order_Invoice_View extends Mage_Adminhtml_Bloc
     {
         parent::__construct();
 
+        // Do nothing if emails are not active
+        if (!$this->_isAllowedAction('emails')) {
+            return;
+        }
+
         $this->_removeButton('send_notification');
+
+        $this->addButton('send_notification', array(
+            'label'     => Mage::helper('sales')->__('Send Email'),
+            'onclick'   => 'confirmSetLocation(\''
+            . Mage::helper('sales')->__('Are you sure you want to send Receipt email to customer?')
+            . '\', \'' . $this->getEmailUrl() . '\')'
+        ));
     }
 }
