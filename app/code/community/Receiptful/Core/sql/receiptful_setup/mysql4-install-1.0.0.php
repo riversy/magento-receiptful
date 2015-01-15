3c6b62bb49c1819db4ae5c85277aa089913b7d35
Stefano Sala <stefano.sala@spa.it>
2015-01-15 17:00:58 +0100

    Added header to install file

--- app/code/community/Receiptful/Core/sql/receiptful_setup/mysql4-install-1.0.0.php
+++ app/code/community/Receiptful/Core/sql/receiptful_setup/mysql4-install-1.0.0.php
@@ -1,4 +1,14 @@
 <?php
+/**
+ * This file is part of the Receiptful extension.
+ *
+ * (c) Receiptful <info@receiptful.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Stefano Sala <stefano@receiptful.com>
+ */
 $installer = $this;
 $installer->startSetup();
 
