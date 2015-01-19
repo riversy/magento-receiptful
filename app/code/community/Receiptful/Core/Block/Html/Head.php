99f5d912de7da3c2c3a3836c13a23ff4d0ead3dc
Stefano Sala <stefano.sala@spa.it>
2015-01-19 11:58:08 +0100

    Added tracking script and first tracking call

---
+++ app/code/community/Receiptful/Core/Block/Html/Head.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * This file is part of the Receiptful extension.
+ *
+ * (c) Receiptful <info@receiptful.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Stefano Sala <stefano@receiptful.com>
+ */
+class Receiptful_Core_Block_Html_Head extends Mage_Page_Block_Html_Head
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function _construct()
+    {
+        parent::_construct();
+
+        $this->addItem('external_js', 'https://app.receiptful.com/scripts/tracking.js');
+        $this->addItem('js_text', 'Receiptful.setTrackingCookie();');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function _separateOtherHtmlHeadElements(&$lines, $itemIf, $itemType, $itemParams, $itemName, $itemThe)
+    {
+        if ('external_js' === $itemType) {
+            return $lines[$itemIf]['other'][] = sprintf('<script type="text/javascript" src="%s" %s></script>', $itemName, $params);
+        }
+
+        if ('js_text' === $itemType) {
+            return $lines[$itemIf]['other'][] = sprintf('<script type="text/javascript" %s>%s</script>', $params, $itemName);
+        }
+
+        return parent::_separateOtherHtmlHeadElements($lines, $itemIf, $itemType, $itemParams, $itemName, $itemThe);
+    }
+}
